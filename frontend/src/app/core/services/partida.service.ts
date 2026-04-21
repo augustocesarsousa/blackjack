3fdd4d4dfc14c7e59afb3669e61008129e061cb4
Augusto Cesar <acs-03@hotmail.com>
2026-04-21 15:37:15 -0300

    frontend - adiciona o service para fazer as requisições http ao backend

---
+++ frontend/src/app/core/services/partida.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Partida } from '../../shared/models/partida.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PartidaService {
+  private readonly http = inject(HttpClient);
+
+  private readonly API_URL = 'http://localhost:8080/api/partida';
+
+  novoJogo(): Observable<Partida> {
+    return this.http.post<Partida>(`${this.API_URL}/novo-jogo`, {});
+  }
+
+  obterPartidaAtual(): Observable<Partida> {
+    return this.http.get<Partida>(this.API_URL);
+  }
+
+  pedirCarta(): Observable<Partida> {
+    return this.http.patch<Partida>(`${this.API_URL}/pedir-carta`, {});
+  }
+
+  parar(): Observable<Partida> {
+    return this.http.patch<Partida>(`${this.API_URL}/parar`, {});
+  }
+}
